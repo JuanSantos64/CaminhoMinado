aa86f9c787325a55b80aec9f8f16cd0d2219cf10
Juan Santos <107580945+JuanSantos64@users.noreply.github.com>
2023-11-10 20:54:28 -0300

    Update jogo.java

--- jogo.java
+++ jogo.java
@@ -15,43 +15,72 @@ public class jogo {
         int num = entrada.nextInt();
         System.out.println("Digite o numero de bombas");
         int bomba = entrada.nextInt();
-
+        if(num > 1 && bomba > 1){
         ArrayList<Integer> caminhoMinado = new ArrayList<>();
         for (int i = 0; i < (num - bomba); i++) {
             caminhoMinado.add(0);
         }
         for (int a = 0; a < bomba; a++) {
-            caminhoMinado.add(1);
+            caminhoMinado.add(-1);
         }
         Collections.shuffle(caminhoMinado);
         Collections.shuffle(caminhoMinado);
         Collections.shuffle(caminhoMinado);
+        ArrayList<String> caminhoMinadoUsuario = new ArrayList<>();
         for (int i = 0; i < caminhoMinado.size(); i++) {
-            System.out.print(" _ ");
+            caminhoMinadoUsuario.add("_ ");
         }
+        
         int pontos = 0;
         System.out.println("");
         for (int i = 0; i < (caminhoMinado.size() - bomba); i++) {
+            for (String string : caminhoMinadoUsuario) {
+                System.out.print(string);
+            }
+            for (int j = 0; j < caminhoMinado.size(); j++) {
+                System.out.print(caminhoMinado.get(j));
+            }
+            System.out.println("");
             System.out.println("Digite o seu chute entre 1 a " + caminhoMinado.size());
             
             int chute = entrada.nextInt();
-            int valor = caminhoMinado.get(chute - 1);
-            if (valor == 0) {
-                System.out.println("Tá vivo!!!");
-                if (chute < caminhoMinado.size() && caminhoMinado.get(chute) == 1) {
-                    System.out.println("Tem bomba próximo");
-                }
-                if (chute > 1 && caminhoMinado.get(chute - 2) == 1) {
-                    System.out.println("Tem bomba próximo");
-                }
-                pontos++;
-            } else {
-                System.out.println("Morreu pra bomba");
+            if(caminhoMinadoUsuario.get(chute-1).equals("X ")){
+                System.out.println("Você não me engana, você tentou digitar o mesmo número pra acumular pontos, agora você perdeu...");
+                System.out.println("Game Over");
                 break;
             }
+                
+            int valor = caminhoMinado.get(chute - 1);
             
-    }
-        System.out.println("Sua pontuação final é " + pontos + " pontos");
+            if (valor == 0) {
+                System.out.println("Tá vivo!!!");
+                caminhoMinadoUsuario.set(chute-1, "X ");
+                pontos++;
+            }
+            else {
+                System.out.println("Game Over");
+                break;
+            }
+                if (chute < caminhoMinado.size() && caminhoMinado.get(chute) == -1) {
+                    System.out.println("Tem bomba próximo");
+                }
+                else if (chute > 1 && caminhoMinado.get(chute - 2) == -1) {
+                    System.out.println("Tem bomba próximo");
+                }
+                
+            } 
+            
+        if(pontos == (caminhoMinado.size() - bomba)){
+            System.out.println("Você ganhou!");
         }
+        System.out.println("Sua pontuação final é " + pontos + " pontos");
+        System.out.println("Número de bombas: " + bomba);
+        System.out.print("Caminho Minado: ");
+        for (String string : caminhoMinadoUsuario) {
+            System.out.print(string);
+        }
+            System.out.println("");
+        }
+    }
 }
 
